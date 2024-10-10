7e5876984e260519ddb6c74e848ae304c246f017
marksman <m--m@meta.ua>
2024-10-10 20:52:15 +0300

    1-st task end

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -20,3 +20,5 @@ def post_list(request):
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     return render(request, "blog/post/detail.html", {"post": post})
+
+# 1-st task end
